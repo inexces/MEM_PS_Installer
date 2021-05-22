a3e6dc22785c8e86954f95500ad3d9bcda2bde4e
inexces <53097949+inexces@users.noreply.github.com>
2021-05-22 22:11:40 +0200

    1.8b

--- IntuneSetup.ps1
+++ IntuneSetup.ps1
@@ -5,20 +5,27 @@
 .DESCRIPTION
   This script is a framework and standardisation for installing apps focussed on Intune.
   It performs an install or uninstall depending on the "type" parameter
+  Vars
+  $PSScriptRoot
+  $($Settings.config.BrandName)
+  $($Settings.config.App.Packagename)
+  $($Settings.config.App.AppVersion)
+  $($Settings.config.App.PackVersion)
+  $($Settings.config.EventLogSrc)
   
 .INPUTS
   -Type		The type of deployment to perform. Options: [Install, Uninstall]. Default is: Install.
 
 .OUTPUTS
-  Transcript logfile: c:\programdata\$BrandName\Log\$logfile
-  Registry Enry: HKLM\SOFTWARE\$BrandName\Packages\
+  Log file: %SystemRoot%\System32\Winevt\Logs\NAME.evtx
+  Registry Enry: HKLM\SOFTWARE\$($Settings.config.BrandName)\Packages\
 
 .EXAMPLE
 	powershell.exe -executionpolicy bypass -noprofile -noninteractive -file ".\IntuneSetup.ps1 -Type "Install""
 	powershell.exe -executionpolicy bypass -noprofile -noninteractive -file ".\IntuneSetup.ps1 -Type "Uninstall"
 	
 .NOTES
-  Version:        1.7
+  Version:        1.8
   Author:         Marcus Jaken ~ Microsoft Cloud Consultant @ Advantive B.V
   Creation Date:  2021-05-19
   
@@ -26,132 +33,117 @@
 		
 #--------------------------------------------------[Run script in 64bit]-----------------------------------------------------------
 
-	If ($ENV:PROCESSOR_ARCHITEW6432 -eq "AMD64") {
-		Try {
-			&"$ENV:WINDIR\SysNative\WindowsPowershell\v1.0\PowerShell.exe" -File $PSCOMMANDPATH
-		}
-		Catch {
-			Throw "Failed to start $PSCOMMANDPATH"
-		}
-		Exit
-	}
+If ($ENV:PROCESSOR_ARCHITEW6432 -eq "AMD64") {
+	Try { &"$ENV:WINDIR\SysNative\WindowsPowershell\v1.0\PowerShell.exe" -File $PSCOMMANDPATH }
+	Catch { Throw "Failed to start $PSCOMMANDPATH" }
+	Exit
+}
 
 #---------------------------------------------------------[Input]------------------------------------------------------------------
 
-	#Set Default parameter
-	Param (
-		$Type = 'Install'
-	)
+#Set Default parameter
+#	Param(
+#		[Parameter(Mandatory=$True)][String]$Type
+#        $Type = "Install"
+#	)
 
-	## Load the Config.xml
-	[Xml]$config = Get-Content "$($exeScriptDir)IntuneConfig.xml"
-        $BrandName = "$($config.config.BrandName)"
-        $Packagename = "$($config.config.Packagename)"
-        $Version = "$($config.config.Version)"
-        $installdate = Get-Date -Format "yyyy/MM/dd HH:mm"
-        $exeScriptDir = $PSScriptRoot
-        $logfile = "$Packagename" + "_" + "$Version.log"
-        $RegkeyName = "$Packagename" + "_" + "$Version"
-	Write-Host "Setting adjustable Variables $($config.config.BrandName), $($config.config.Packagename), $($config.config.Version)", "Install folder: $exeScriptDir", "Loading configuration: $($exeScriptDir)Config.xml"
+## Load the Config.xml
+[Xml]$Settings = Get-Content "$($PSScriptRoot)\IntuneConfig.xml"
+$Present = Get-Date -Format "yyyy/MM/dd HH:mm"
+$Package = $($Settings.config.App.Packagename) + " " + $($Settings.config.App.AppVersion)
+$MessageInput =  "Setting Variables for $($Settings.config.BrandName); $($Settings.config.App.Packagename), $($Settings.config.App.AppVersion), Install folder: $PSScriptRoot"
 
 #-----------------------------------------------------[Initialisation]-------------------------------------------------------------
 
-	#Create Log-directory
-	if(!(Test-Path "$($env:ProgramData)\$BrandName")) { 
-        New-Item -Path "$($env:ProgramData)" -Name "$BrandName" -ItemType "directory" -Force -ErrorAction SilentlyContinue
-    }    
-	
+#Create Company-directory & Eventlog
+New-EventLog -LogName $($Settings.config.BrandName) -Source $Package -ErrorAction SilentlyContinue
+# if(!(Test-Path "$($env:ProgramData)\$($Settings.config.BrandName)")) { 
+# New-Item -Path "$($env:ProgramData)" -Name "$($Settings.config.BrandName)" -ItemType "directory" -Force -ErrorAction SilentlyContinue
+# $MessageInitialisation = "Created `"$($env:ProgramData)\$($Settings.config.BrandName)`""
+# }    
+
 #-----------------------------------------------------------[Functions]------------------------------------------------------------
 
-	# Registry Module
-	Function RegisterInstallation() {
-		Param(
-			[Parameter(Mandatory=$True)][String]$ErrorLevel
+Function WriteEventlog {
+		Param ( 
+			[Parameter(Mandatory=$true)]
+			[string]$GetMessage
 		)
-		If ($ErrorLevel -eq "0") {
-			Write-Host "Create Registry installation key HKLM\SOFTWARE\$BrandName\Packages\$RegkeyName"
-			New-Item -Path Registry::"HKLM\SOFTWARE\$BrandName\Packages\$RegkeyName" -type Directory -Force -ErrorAction SilentlyContinue
-			new-itemproperty Registry::"HKLM\SOFTWARE\$BrandName\Packages\$RegkeyName" -Name "Version" -Value $Version -PropertyType String -Force -ErrorAction SilentlyContinue
-			new-itemproperty Registry::"HKLM\SOFTWARE\$BrandName\Packages\$RegkeyName" -Name "Install_Date" -Value $installdate -PropertyType String -Force -ErrorAction SilentlyContinue
-		} 
-    EXIT $ErrorLevel
-	} 
-	
-	Function UnregisterInstallation() {
-		Param(
-			[Parameter(Mandatory=$True)][String]$ErrorLevel
-		)
-		If ($ErrorLevel -eq "0") {
-			Write-Host "Delete Registry installation key HKLM\SOFTWARE\$BrandName\Packages\$RegkeyName"
-			Remove-Item -Path "HKLM:\SOFTWARE\$BrandName\Packages\$RegkeyName" -Force
-	} 
-    EXIT $ErrorLevel
-	} 	
-	
+		$writemessage = "IntuneSetup: " + $Package + " - " + $GetMessage
+		Write-EventLog -LogName $($Settings.config.BrandName) -Source $Package -EventID 1 -EntryType "Information" -Message $writemessage -Category 1
+}
+
+Function RegisterInstallation() {
+	Param(
+		[Parameter(Mandatory=$True)][String]$ErrorLevel
+	)
+	If ($ErrorLevel -eq "0") {
+		WriteEventlog -GetMessage "Create Registry installation key HKLM\SOFTWARE\$($Settings.config.BrandName)\Packages\$Package"
+		New-Item -Path Registry::"HKLM\SOFTWARE\$($Settings.config.BrandName)\Packages\$($Settings.config.App.Packagename)\$($Settings.config.App.AppVersion)" -type Directory -Force -ErrorAction SilentlyContinue
+		new-itemproperty Registry::"HKLM\SOFTWARE\$($Settings.config.BrandName)\Packages\$($Settings.config.App.Packagename)\$($Settings.config.App.AppVersion)" -Name "Installed" -Value $Present -PropertyType String -Force -ErrorAction SilentlyContinue
+	} Else {
+		WriteEventlog -GetMessage "Error during installation $Error, total errors: $($error.count)"
+	}
+EXIT $ErrorLevel
+} 
+
+Function UnregisterInstallation() {
+	Param(
+		[Parameter(Mandatory=$True)][String]$ErrorLevel
+	)
+	If ($ErrorLevel -eq "0") {
+		WriteEventlog -GetMessage "Delete Registry installation key HKLM\SOFTWARE\$($Settings.config.BrandName)\Packages\$Package"
+		Remove-Item -Path "HKLM:\SOFTWARE\$($Settings.config.BrandName)\Packages\$Package" -Force
+} 
+EXIT $ErrorLevel
+} 	
 
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
 
-	## Do not remove, setting error level to zero before installation
-        $Error.Clear()
+WriteEventlog -GetMessage $MessageInput
 
-	If ($Type -ine 'Uninstall') {
-		Start-Transcript "$($env:ProgramData)\$BrandName\Log\$Type-$logfile"
-		
-		#[Prerequisits]------------------------------------------------------------------------------------------------------------
-		
-		#[/Prerequisits]-----------------------------------------------------------------------------------------------------------
-		
-		#[Instalation]-------------------------------------------------------------------------------------------------------------
-		
-		Function Install {
-			Write-Host "Teamviewer installer Host 15.17.6"
-			$msifile = "$($exeScriptDir)\TeamViewer_Host.msi"
-				$msiargs = @(
-					"/i"
-					"`"$msifile`""
-					'APITOKEN='
-					'CUSTOMCONFIGID='
-					'DESKTOPSHORTCUTS=0'
-					"SETTINGSFILE=`"$($exeScriptDir)\2021.tvopt`""
-					"/qn"
-				)
-			(start-process msiexec.exe -ArgumentList $msiargs -wait -PassThru).ExitCode
-			Start-Sleep -s 10
-		}
-		Install
-				
-		#[/Instalation]------------------------------------------------------------------------------------------------------------
-		
-		If ($Error.Count -gt 0) {
-			RegisterInstallation -ErrorLevel 101
-		} else {
-		}
-		
+## Do not remove, setting error level to zero before installation
+$Error.Clear()
+
+If ($Type -ine 'Uninstall') {
+	
+																			  WriteEventlog -GetMessage "Starting Prerequisits"
+	#[Prerequisits]----------------------------------------------------------------------------------------------[Prerequisits]
+
+	#[/Prerequisits]--------------------------------------------------------------------------------------------[/Prerequisits]
+																			 WriteEventlog -GetMessage "Finishing Prerequisits"
+	
+																			  WriteEventlog -GetMessage "Starting Installation"
+	#[Instalation]------------------------------------------------------------------------------------------------[Instalation]
+	New-EventLog -LogName $($Settings.config.BrandName) -Source $Package			
+	#[/Instalation]----------------------------------------------------------------------------------------------[/Instalation]
+																			 WriteEventlog -GetMessage "Finishing Installation"		
+
+	If ($Error.Count -gt 0) {
+		RegisterInstallation -ErrorLevel 101
+	} else {
 		RegisterInstallation -ErrorLevel 0
-
-		Stop-Transcript
 	}
-	ElseIf ($Type -ieq 'Uninstall') {
-		Start-Transcript "$($env:ProgramData)\$BrandName\Log\$Type-$logfile"
-		
-		#[Uninstallation]----------------------------------------------------------------------------------------------------------
-		
-		Function Uninstall {
-			Start-Process -FilePath "msiexec.exe" -ArgumentList "/X{F9631805-F9F3-457E-B213-5E3213C1C16C} /qn" -Wait -Passthru
-		}
-		Uninstall
-		
-		#[/Uninstallation]---------------------------------------------------------------------------------------------------------
-		
-		If ($Error.Count -gt 0) {
-			UnregisterInstallation -ErrorLevel 101
-		} else {
-		}
-		
-		UnregisterInstallation -ErrorLevel 0		
 
-		Stop-Transcript
+}
+ElseIf ($Type -ieq 'Uninstall') {
+	
+																			WriteEventlog -GetMessage "Strating Uninstallation"
+	#[Uninstallation]------------------------------------------------------------------------------------------[Uninstallation]
+			
+	#[/Uninstallation]----------------------------------------------------------------------------------------[/Uninstallation]
+																		   WriteEventlog -GetMessage "Finishing Uninstallation"
+
+	If ($Error.Count -gt 0) {
+		UnregisterInstallation -ErrorLevel 101
+	} else {
+		UnregisterInstallation -ErrorLevel 0	
 	}
 	
+
+
+}
+
 #------------------------------------------------------------[Exiting]-------------------------------------------------------------
+
 
